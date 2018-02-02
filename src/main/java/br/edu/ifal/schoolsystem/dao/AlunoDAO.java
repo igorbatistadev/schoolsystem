0ca7bd4b0e8c081e9b38485acf746af094cc2f3b
Igor Santana <igoricm1@gmail.com>
2018-02-02 16:41:34 -0300

    Implementação do DAO de aluno

--- src/main/java/br/edu/ifal/schoolsystem/dao/AlunoDAO.java
+++ src/main/java/br/edu/ifal/schoolsystem/dao/AlunoDAO.java
@@ -1,16 +1,72 @@
 package br.edu.ifal.schoolsystem.dao;
 
 import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import br.edu.ifal.schoolsystem.modelo.Aluno;
 
 public class AlunoDAO implements DAOInterface <Aluno, String>{
+	
+	private Session currentSession;
+	
+	private Transaction currentTransaction;
 
-	public void salver(Aluno entity) {
-		// TODO Auto-generated method stub
-		
+	public AlunoDAO() {
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+	
+	public Session openCurrentSession() {
+		currentSession = getSessionFactory().openSession();
+		return currentSession;
+	}
+	
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+	
+	public void closeCurrentSession() {
+		currentSession.close();
 	}
 
-	public void altualizar(Aluno entity) {
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}
+	
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void salvar(Aluno entity) {
+		getCurrentSession().save(entity);		
+	}
+
+	public void atualizar(Aluno entity) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/main/java/br/edu/ifal/schoolsystem/dao/DAOInterface.java
+++ src/main/java/br/edu/ifal/schoolsystem/dao/DAOInterface.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public interface DAOInterface<T, Id extends Serializable> {	
 
-	public void salver(T entity);
-	public void altualizar(T entity);
+	public void salvar(T entity);
+	public void atualizar(T entity);
 	public T buscarPorId(Id id);
 	public void deletar(T enity);
 	public List<T> buscarTodos();
