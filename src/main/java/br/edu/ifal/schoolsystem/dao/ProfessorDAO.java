2f89293e8115e1f9dcd68127768e9b9b32a332fd
rodrigo <rodrigocr7santista@gmail.com>
2018-02-09 09:13:19 -0300

    Criação do Professor DAO

---
+++ src/main/java/br/edu/ifal/schoolsystem/dao/ProfessorDAO.java
@@ -0,0 +1,94 @@
+package br.edu.ifal.schoolsystem.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import br.edu.ifal.schoolsystem.modelo.Professor;
+
+public class ProfessorDAO implements DAOInterface <Professor, String>{
+	
+	private Session currentSession;
+	
+	private Transaction currentTransaction;
+
+	public ProfessorDAO() {
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		Configuration configuration = new Configuration().configure();
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory sessionFactory = configuration.buildSessionFactory(builder.build());
+		return sessionFactory;
+	}
+	
+	public Session openCurrentSession() {
+		currentSession = getSessionFactory().openSession();
+		return currentSession;
+	}
+	
+	public Session openCurrentSessionwithTransaction() {
+		currentSession = getSessionFactory().openSession();
+		currentTransaction = currentSession.beginTransaction();
+		return currentSession;
+	}
+	
+	public void closeCurrentSession() {
+		currentSession.close();
+	}
+
+	public void closeCurrentSessionwithTransaction() {
+		currentTransaction.commit();
+		currentSession.close();
+	}
+	
+	public Session getCurrentSession() {
+		return currentSession;
+	}
+
+	public void setCurrentSession(Session currentSession) {
+		this.currentSession = currentSession;
+	}
+
+	public Transaction getCurrentTransaction() {
+		return currentTransaction;
+	}
+
+	public void setCurrentTransaction(Transaction currentTransaction) {
+		this.currentTransaction = currentTransaction;
+	}
+
+	public void salvar(Professor entity) {
+		getCurrentSession().save(entity);		
+	}
+
+	public void atualizar(Professor entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Professor buscarPorId(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deletar(Professor enity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Professor> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deletarTodos() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
