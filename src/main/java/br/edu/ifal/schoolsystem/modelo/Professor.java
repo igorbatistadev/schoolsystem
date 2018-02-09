01eea89ab55c0a01b487c378e53b5347279ae47a
rodrigo <rodrigocr7santista@gmail.com>
2018-02-09 11:28:06 -0300

    inseriu anotaçoes do hiberbate 5

--- src/main/java/br/edu/ifal/schoolsystem/modelo/Professor.java
+++ src/main/java/br/edu/ifal/schoolsystem/modelo/Professor.java
@@ -2,11 +2,16 @@ package br.edu.ifal.schoolsystem.modelo;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name  = "professor")
+@Table(name ="professor")
 public class Professor {
+	
+	@Id
+	private int id;
+	
 	@Column
 	private String nome;
 	
@@ -18,6 +23,20 @@ public class Professor {
 		nome = "";
 		formacao = "";
 	}
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
 	public String getNome() {
 		return nome;
 	}
