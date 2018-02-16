16e6dd9c438b1a67c1a58a6790d6faedb2a521f2
Igor Santana <igoricm1@gmail.com>
2018-02-16 16:06:59 -0300

    Alteração do arquivo pom para que o sistema funcionasse

--- src/main/java/br/edu/ifal/schoolsystem/app/Main.java
+++ src/main/java/br/edu/ifal/schoolsystem/app/Main.java
@@ -1,15 +1,24 @@
 package br.edu.ifal.schoolsystem.app;
 
 import br.edu.ifal.schoolsystem.dao.AlunoDAO;
+import br.edu.ifal.schoolsystem.dao.ProfessorDAO;
 import br.edu.ifal.schoolsystem.modelo.Aluno;
+import br.edu.ifal.schoolsystem.modelo.Professor;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Aluno aluno = new Aluno(10, "1023a120", "Rodrigo Rodrigues");
+		/*Aluno aluno = new Aluno(11, "2223a120", "Daniel Rodrigues");
 		AlunoDAO dao = new AlunoDAO();
-		dao.salvar(aluno);
+		dao.salvar(aluno);*/
 		
+		
+		Professor professor = new Professor();
+		//professor.setId(1);
+		//professor.setNome("Leonardo bruno");
+		professor.setFormacao("Doutor em Eletronica");
+		ProfessorDAO daoP = new ProfessorDAO();
+		daoP.salvar(professor);
 		//
 
 	}
