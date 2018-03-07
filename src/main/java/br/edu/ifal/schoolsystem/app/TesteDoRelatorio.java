a27826d4ca39c1919318d0893b3c65e7fb0aa093
igor <igoricm1@gmail.com>
2018-03-07 15:03:35 -0300

    Criação da Classe TesteDoRelatorio

---
+++ src/main/java/br/edu/ifal/schoolsystem/app/TesteDoRelatorio.java
@@ -0,0 +1,37 @@
+package br.edu.ifal.schoolsystem.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifal.schoolsystem.modelo.Aluno;
+import br.edu.ifal.schoolsystem.modelo.Disciplina;
+import br.edu.ifal.schoolsystem.modelo.Nota;
+import br.edu.ifal.schoolsystem.servico.Relatorio;
+
+public class TesteDoRelatorio {
+
+	public static void main(String[] args) {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(5, "2018", "ze");
+		Aluno ana = new Aluno(6, "2019", "ana");
+		Aluno joao = new Aluno(3, "2017", "joao");
+		alunos.add(ze);
+		alunos.add(ana);
+		alunos.add(joao);
+		
+		Disciplina poo = new Disciplina (3,"POO",alunos,null);
+		
+		List <Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1,ze,poo,7.5));
+		
+		notas.add(new Nota(2,ana,poo,9.5));
+		
+		notas.add(new Nota(3,joao,poo,2.5));
+		
+		Relatorio relatorio = new Relatorio ();
+		relatorio.gerar(notas);
+		
+		System.out.println(relatorio.getMaiorNota());
+	}
+
+}
