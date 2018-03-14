de638820fb3f08ccab4010894dce8b7b2d0e1862
janaina <janainarodriguesdemiranda@gmail.com>
2018-03-14 16:36:06 -0300

    testes e modificações para o melhoramento do codigo

--- src/main/java/br/edu/ifal/schoolsystem/servico/Relatorio.java
+++ src/main/java/br/edu/ifal/schoolsystem/servico/Relatorio.java
@@ -1,14 +1,17 @@
 package br.edu.ifal.schoolsystem.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-
-import javax.management.NotificationBroadcaster;
+import java.util.stream.Collectors;
 
 import br.edu.ifal.schoolsystem.modelo.Nota;
 
 public class Relatorio {
 	private double maiorNota = Double.NEGATIVE_INFINITY;
 	private double menorNota = Double.POSITIVE_INFINITY;
+	private List<Nota> top3Notas = new ArrayList<Nota>();
 
 	
 	
@@ -17,10 +20,19 @@ public class Relatorio {
 		for (Nota nota : notas) {
 			if(nota.getValor() > maiorNota) {
 				maiorNota = nota.getValor();
-			} if(nota.getValor() < menorNota) {
+			}
+			if(nota.getValor() < menorNota) {
 				menorNota = nota.getValor();
 			}
 		}
+		
+		setarMaioresNotas(notas);
+	}
+
+
+	private void setarMaioresNotas(List<Nota> notas) {
+		List<Nota> notasOrdemDecrescente = notas.stream().sorted(Comparator.comparing(Nota::getValor)).collect(Collectors.toList());
+		Collections.reverse(notasOrdemDecrescente);
 	}
 
 
--- src/test/java/br/edu/ifal/schoolsystem/servico/RelatorioTest.java
+++ src/test/java/br/edu/ifal/schoolsystem/servico/RelatorioTest.java
@@ -1,6 +1,7 @@
 package br.edu.ifal.schoolsystem.servico;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,7 +16,7 @@ import br.edu.ifal.schoolsystem.modelo.Nota;
 public class RelatorioTest {
 
 	@Test
-	public void test() {
+	public void deveFuncionarParaAsNotasNaOrdemAleatoria() {
 		List<Aluno> alunos = new ArrayList<Aluno>();
 		Aluno ze = new Aluno(5, "2018", "ze");
 		Aluno ana = new Aluno(6, "2019", "ana");
@@ -39,13 +40,104 @@ public class RelatorioTest {
 		double maiorNotaEsperada = 9.5;
 		double menorNotaEsperada = 2.5;
 		
-		Assert.assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
-		Assert.assertEquals(menorNotaEsperada, relatorio.getMenorNota(),0000.1);
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(),0000.1);
 		
 		
-		//System.out.println(relatorio.getMaiorNota() == maiorNotaEsperada);
 		
-		//System.out.println(relatorio.getMenorNota() == menorNotaEsperada);
 	}
+	
+	@Test
+	public void deveFuncionarParaAsNotasNaOrdemCrescente() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(5, "2018", "ze");
+		Aluno ana = new Aluno(6, "2019", "ana");
+		Aluno joao = new Aluno(3, "2017", "joao");
+		alunos.add(ze);
+		alunos.add(ana);
+		alunos.add(joao);
+		
+		Disciplina poo = new Disciplina (3,"POO",alunos,null);
+		
+		List <Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1,ze,poo,2.5));
+		
+		notas.add(new Nota(2,ana,poo,7.5));
+		
+		notas.add(new Nota(3,joao,poo,9.5));
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+		
+		double maiorNotaEsperada = 9.5;
+		double menorNotaEsperada = 2.5;
+		
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(),0000.1);
+		
+		
+		
+	}
+	
+	@Test
+	public void deveFuncionarParaAsNotasNaOrdemDecrescente() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(5, "2018", "ze");
+		Aluno ana = new Aluno(6, "2019", "ana");
+		Aluno joao = new Aluno(3, "2017", "joao");
+		alunos.add(ze);
+		alunos.add(ana);
+		alunos.add(joao);
+		
+		Disciplina poo = new Disciplina (3,"POO",alunos,null);
+		
+		List <Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1,ze,poo,9.5));
+		
+		notas.add(new Nota(2,ana,poo,7.5));
+		
+		notas.add(new Nota(3,joao,poo,2.5));
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+		
+		double maiorNotaEsperada = 9.5;
+		double menorNotaEsperada = 2.5;
+		
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+		
+		
+		
+	}
+	
+	@Test
+	public void deveFuncionarParaUmUnicoAluno() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		Aluno ze = new Aluno(5, "2018", "ze");
+		
+		alunos.add(ze);
+		
+		
+		Disciplina poo = new Disciplina (3,"POO",alunos,null);
+		
+		List <Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1,ze,poo,2.5));
+		
+		
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+		
+		double maiorNotaEsperada = 2.5;
+		double menorNotaEsperada = 2.5;
+		
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+		
+		
+		
+	}
+
 
 }
